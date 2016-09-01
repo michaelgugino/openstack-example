36e5feb485863f550e4f263e015d1945b5365f26
Michael Gugino <michael.gugino@walmart.com>
2016-09-01 15:47:51 -0400

    sd.py

--- sd.py
+++ sd.py
@@ -1,6 +1,15 @@
+import argparse
 import stevedore
 
-exts = stevedore.ExtensionManager(namespace='exampleclient.example')
+exts = stevedore.ExtensionManager(namespace='exampleclient.example.hello')
 
-fun1 = exts['sayhello']
-fun1()
+funs = exts.names()
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('command', metavar='C')
+args = parser.parse_args()
+try:
+    exts[args.command].plugin()
+except:
+    print("Command %s not found, valid commands are: %s" % (args.command, funs))
